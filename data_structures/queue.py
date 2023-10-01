05d80c606a40567653e579a2d00bdeb3e9a2f847
Dominic Galiano <dgaliano@utexas.edu>
2023-10-01 18:39:32 -0400

    queue placehodler

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,3 +1,24 @@
+# Using python native linked list to eliminate
+from collections import deque
+
+
 class Queue:
     def __init__(self):
+        self.queue = deque
+
+    def __str__(self):
+        output = ""
+
+        for element in q:
+            output += f"{element} "
+
+        return output.strip()
+
+    def enqueue(self, value):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
         pass
