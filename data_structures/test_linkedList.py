32a1e631a8c5d27261cbf757c0babc78b5301abe
Dominic Galiano <dgaliano@utexas.edu>
2023-10-01 16:08:43 -0400

    add gitignore and test framework

---
+++ data_structures/test_linkedList.py
@@ -0,0 +1,13 @@
+import unittest
+from linkedList import SinglyLinkedList
+
+
+class TestSinglyLinkedListMethods(unittest.TestCase):
+    def test_append(self):
+        testList = SinglyLinkedList()
+        testList.append(10)
+        self.assertEqual(str(testList), "10")
+
+
+if __name__ == "__main__":
+    unittest.main()
