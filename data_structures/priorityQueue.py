856d250d9e0998dfe8954eb3525e6570737d6c2a
Dominic Galiano <dgaliano@utexas.edu>
2023-10-01 21:57:21 -0400

    implemented queue tests

--- data_structures/priorityQueue.py
+++ data_structures/priorityQueue.py
@@ -2,3 +2,58 @@ class PQueue:
     def __init__(self):
         self.heap = []
         self.heapSize = 0
+
+    def __str__(self):
+        return str(self.heap)
+
+    def isEmpty(self):
+        return self.heapSize == 0
+
+    def clear(self):
+        self.heap = []
+
+    def heapify(self):
+        """O(n) efficient heap formation"""
+        raise NotImplementedError("Not implemented for time sake")
+
+    def size(self):
+        return self.heapSize
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.heap[0]
+
+    def poll(self):
+        return self.removeAt(0)
+
+    def contains(self, element):
+        for heapElement in self.heap:
+            if element == heapElement:
+                return True
+        return False
+
+    def removeAt(self, index):
+        pass
+
+    def add(self, element):
+        if element is None:
+            raise TypeError("Element cannot be None")
+
+        self.heap.append(element)
+        self.swim(self.heapSize)
+        self.heapSize += 1
+
+    def swim(self, k):
+        # bottom up node swim, O(log(n))
+        parent = (k - 1) // 2
+        while k > 0 and self.less(k, parent):
+            # bubble up
+            self.heap[k], self.heap[parent] = self.heap[parent], self.heap[k]
+            k = parent
+
+            # recalculate parent
+            parent = (k - 1) // 2
+
+    def less(self, i, j):
+        return self.heap[i] <= self.heap[j]
--- data_structures/test_priorityQueue.py
+++ data_structures/test_priorityQueue.py
@@ -3,8 +3,56 @@ from priorityQueue import PQueue
 
 
 class TestPriorityQueueMethods(unittest.TestCase):
-    def test_enqueue(self):
-        pass
+    def test_add_sequential(self):
+        pq = PQueue()
+        pq.add(1)
+        pq.add(2)
+        pq.add(3)
+        self.assertEqual(pq.heap, [1, 2, 3])
+
+    def test_add_nonsequential(self):
+        pq = PQueue()
+        pq.add(10)
+        pq.add(9)
+        pq.add(8)
+        pq.add(7)
+        self.assertEqual(pq.heap, [7, 8, 9, 10])
+
+    def test_peek(self):
+        pq = PQueue()
+        pq.add(10)
+        pq.add(9)
+        pq.add(8)
+        pq.add(7)
+        self.assertEqual(pq.peak(), 7)
+
+    def test_poll(self):
+        pq = PQueue()
+        pq.add(10)
+        pq.add(9)
+        pq.add(8)
+        pq.add(7)
+        element = pq.poll()
+        self.assertEqual(element, 7)
+        self.assertEqual(pq.heap, [8, 10, 9])
+
+    def test_removeAt(self):
+        pq = PQueue()
+        pq.add(1)
+        pq.add(2)
+        pq.add(3)
+        pq.add(4)
+        pq.add(5)
+        pq.add(6)
+        pq.add(7)
+        pq.add(8)
+        pq.add(9)
+        # [1,2,3,4,5,6,7,8,9]
+        pq.removeAt(3)
+        # [1,2,3,none,5,6,7,8,9]
+        # [1,2,3,9,5,6,7,8]
+        # [1,2,3,7,5,6,9,8]
+        self.assertEqual(pq.heap, [1, 2, 3, 7, 5, 6, 9, 8])
 
 
 if __name__ == "__main__":
