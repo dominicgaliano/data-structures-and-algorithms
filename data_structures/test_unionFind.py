230542d9db3d03dc3aa478ad00dc1f8e62256827
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 10:27:20 -0400

    partial test cases

--- data_structures/test_unionFind.py
+++ data_structures/test_unionFind.py
@@ -3,7 +3,20 @@ from unionFind import UnionFind
 
 
 class TestUnionFindMethods(unittest.TestCase):
-    def test_union(self):
+    def test_unionFind_init(self):
+        elements = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        uf = UnionFind(elements)
+        self.assertEqual(str(uf), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_unionFind_init_set(self):
+        elements = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
+        uf = UnionFind(elements)
+        self.assertTrue(set(uf.arr) == set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+    def test_unionFind_union(self):
+        elements = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        uf = UnionFind(elements)
+        uf.union("A", "B")
         pass
 
 
