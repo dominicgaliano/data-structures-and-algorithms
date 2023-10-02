1be8ca434bd12e544a7c7adb873d34f969a52124
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 10:18:47 -0400

    added init Union Find code

--- data_structures/unionFind.py
+++ data_structures/unionFind.py
@@ -1,6 +1,19 @@
 class UnionFind:
-    def __init(self):
-        pass
+    def __init__(self, elements):
+        try:
+            iter(elements)
+        except TypeError as te:
+            print(elements, "is not iterable")
+
+        self.map = dict()
+        self.arr = [i for i in range(len(elements))]
+        i = 0
+        for element in elements:
+            self.map["element"] = i
+            i += 1
+
+    def __str__(self):
+        return str(self.arr)
 
     def union(self, A, B):
         pass
