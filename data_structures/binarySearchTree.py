6e25201398224f34451c84c613470092ff59dcb4
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 12:33:31 -0400

    implemented passing BST functionality

--- data_structures/binarySearchTree.py
+++ data_structures/binarySearchTree.py
@@ -3,20 +3,36 @@ from nodes import BinaryTreeNode
 
 class BST:
     def __init__(self):
-        root = None
+        self.root = None
+        self.nodeCount = 0
 
     def isEmpty(self):
         return self.root is None
 
     def size(self):
-        pass
+        return self.nodeCount
 
     def add(self, elem):
-        self.addRecursively(self.root, elem)
+        # if contains already, do not add
+        if self.contains(elem):
+            return False
+
+        # recurse
+        self.root = self.addRecursively(self.root, elem)
+        self.nodeCount += 1
 
     # private function to recursively add element
     def addRecursively(self, node, elem):
-        pass
+        # Null node found, place child
+        if node is None:
+            return BinaryTreeNode(elem)
+
+        if elem < node.value:
+            node.left = self.addRecursively(node.left, elem)
+            return node
+
+        node.right = self.addRecursively(node.right, elem)
+        return node
 
     def remove(self, elem):
         pass
@@ -26,20 +42,59 @@ class BST:
         pass
 
     def findMin(self):
-        pass
+        if self.root is None:
+            return None
+
+        # keep going left
+        node = self.root
+        while node.left:
+            node = node.left
+
+        return node.value
 
     def findMax(self):
-        pass
+        if self.root is None:
+            return None
+
+        # keep going right
+        node = self.root
+        while node.right:
+            node = node.right
+
+        return node.value
 
     def contains(self, elem):
-        pass
+        return self.containsRecursively(self.root, elem)
 
     # private function to recursively search for element
     def containsRecursively(self, node, elem):
-        pass
+        # null node reached
+        if node is None:
+            return False
+
+        # comparisons
+        # equal
+        if node.value == elem:
+            return True
+        # node less than elem, recurse right
+        elif node.value < elem:
+            return self.containsRecursively(node.right, elem)
+        # node greater than elem, recurse left
+        return self.containsRecursively(node.left, elem)
 
     def height(self):
-        pass
+        return self.heightRecursively(self.root)
+
+    def heightRecursively(self, node):
+        if node is None:
+            return 0
+        return (
+            max(
+                self.heightRecursively(node.left),
+                self.heightRecursively(node.right),
+            )
+            + 1
+        )
 
     def traverse(self, order):
         pass
