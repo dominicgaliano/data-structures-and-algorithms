307f999fe93d404e012254245bc8e14d03f47f3a
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 10:12:52 -0400

    UnionFind structure

---
+++ data_structures/test_unionFind.py
@@ -0,0 +1,11 @@
+import unittest
+from unionFind import UnionFind
+
+
+class TestUnionFindMethods(unittest.TestCase):
+    def test_union(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ data_structures/unionFind.py
@@ -0,0 +1,9 @@
+class UnionFind:
+    def __init(self):
+        pass
+
+    def union(self, A, B):
+        pass
+
+    def find(self, A):
+        pass
