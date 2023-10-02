feb7302486232b0d53d6d79636017d6c70229412
Dominic Galiano <dgaliano@utexas.edu>
2023-10-02 11:42:59 -0400

    added binary tree node

--- data_structures/nodes.py
+++ data_structures/nodes.py
@@ -8,3 +8,10 @@ class BidirectionalNode(Node):
     def __init__(self, value, prev=None, next=None):
         super().__init__(value, next)
         self.prev = prev
+
+
+class BinaryTreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
