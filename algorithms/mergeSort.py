23ecdc76796096af7b6dc60d835cf88713b61d50
Dominic Galiano <dgaliano@utexas.edu>
2023-10-04 14:06:08 -0400

    mergeSort (and gitignore

--- algorithms/mergeSort.py
+++ algorithms/mergeSort.py
@@ -1,2 +1,28 @@
 def mergeSort(lst):
-    return lst
+    # base case
+    if len(lst) < 2:
+        return lst
+
+    lo, hi = 0, len(lst)
+    mid = (hi - lo) // 2
+    left, right = lst[:mid], lst[mid:]
+
+    left, right = mergeSort(left), mergeSort(right)
+
+    left_i = 0
+    right_i = 0
+
+    result = []
+    for _ in range(hi):
+        # left smaller or right exhausted, add to result
+        if (left_i < mid) and (
+            (right_i >= hi - mid) or left[left_i] < right[right_i]
+        ):
+            result.append(left[left_i])
+            left_i += 1
+        # right smaller or left exhausted, add to result
+        else:
+            result.append(right[right_i])
+            right_i += 1
+
+    return result
